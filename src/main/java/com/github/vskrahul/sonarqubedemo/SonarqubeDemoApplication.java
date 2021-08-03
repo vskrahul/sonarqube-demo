9c5866f2082a6013194c5422014bb2aa60a55dac
Rahul Vishvakarma <C-Rahul.Vishvakarma@charter.com>
2021-08-03 12:09:08 +0530

    adding new empty spring boot project

---
+++ src/main/java/com/github/vskrahul/sonarqubedemo/SonarqubeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.vskrahul.sonarqubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vskrahul/sonarqubedemo/SonarqubeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vskrahul.sonarqubedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
